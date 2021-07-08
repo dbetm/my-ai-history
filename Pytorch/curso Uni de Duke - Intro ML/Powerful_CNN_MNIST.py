176c0fb8121c46f08161c01bc56087dbda7a87a3
David <davbetm@gmail.com>
2021-07-08 00:43:05 -0500

    Add powerful CNN for MNIST

--- Pytorch/curso Uni de Duke - Intro ML/Powerful_CNN_MNIST.py
+++ Pytorch/curso Uni de Duke - Intro ML/Powerful_CNN_MNIST.py
@@ -1,14 +1,13 @@
 import os
 
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from tqdm import tqdm, trange
 
-# Load the data
+# LOADING DATA
 dataset_url = '../../../../../ML_DL/datasets/mnist' # edit
 
 if os.path.isdir(dataset_url):
@@ -29,38 +28,52 @@ mnist_test = datasets.MNIST(
     transform=transforms.ToTensor(),
     download=download
 )
+
 train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=100, shuffle=True)
 test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=100, shuffle=False)
 
 num_classes = 10
 
-# Define model
-
+## DEFINING MODEL
 class MNIST_CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
-        self.fc1 = nn.Linear(in_features=7*7*64, out_features=256)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
+
+        self.fc1 = nn.Linear(in_features=4*4*64, out_features=256)
         self.fc2 = nn.Linear(in_features=256, out_features=num_classes)
+
 
     def forward(self, x):
         # conv layer 1
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2)
 
         # conv layer 2
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)
 
-        # fc layer 1
-        x = x.view(-1, 7*7*64)
+        # conv layer 3
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        # conv layer 4
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+
+        # print(x.shape)
+
+        # fully connected layer 1
+        x = x.view(-1, 4*4*64)
         x = self.fc1(x)
         x = F.relu(x)
 
-        # fc layer 2
+        # fully connected layer 2
         x = self.fc2(x)
 
         return x
@@ -68,7 +81,12 @@ class MNIST_CNN(nn.Module):
 model = MNIST_CNN()
 print(model)
 
-# Training
+model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+params = sum([np.prod(p.size()) for p in model_parameters])
+print(params)
+
+
+# TRAINING MODEL
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 epochs = 3
@@ -104,4 +122,4 @@ with torch.no_grad():
 
 print('Test accuracy: {}'.format(correct / total))
 
-# Test accuracy: 0.9918000102043152
+# 0.9891999959945679
