8ea1349df8df896a6408f90c2d5e2171db19c865
David Betancourt <davbetm@gmail.com>
2024-07-15 20:31:51 -0600

    add proposal for sparse matrix multiplication

--- algoexpert/000_sparse_matrix_mult.py
+++ algoexpert/000_sparse_matrix_mult.py
@@ -3,23 +3,23 @@ from typing import Dict
 
 def sparse_matrix_multiplication(matrix_a, matrix_b):
     """My solution - accepted"""
-    n = len(matrix_a)
-    m = len(matrix_a[0])
+    a_num_rows = len(matrix_a)
+    a_num_cols = len(matrix_a[0])
 
-    n2 = len(matrix_b)
-    m2 = len(matrix_b[0])
+    b_num_rows = len(matrix_b)
+    b_num_cols = len(matrix_b[0])
 
     """Matrices A and B can only be multiplied together if the number
     of columns in Matrix A is equal to the number of rows in matrix B.
     """
-    if m != n2:
+    if a_num_cols != b_num_rows:
         return [[]]
 
     # In matrix_b check for each column in which rows there values different to zero
     # then use that info to optimize multiplication
     non_zero_values_per_col: Dict[set] = dict()
-    for col in range(m2):
-        for row in range(n2):
+    for col in range(b_num_cols):
+        for row in range(b_num_rows):
             if matrix_b[row][col] != 0:
                 if col in non_zero_values_per_col:
                     non_zero_values_per_col[col].add(row)
@@ -29,15 +29,55 @@ def sparse_matrix_multiplication(matrix_a, matrix_b):
     num_ops = 0
     # has the number of rows that matrix A has and the number of columns that matrix B has
     ans = list()
-    for i in range(n):
+    for i in range(a_num_rows):
         row = list()
-        for col_idx in range(m2):
+        for col_idx in range(b_num_cols):
             total = 0
             for jdx in non_zero_values_per_col.get(col_idx, []):
                 total += matrix_a[i][jdx] * matrix_b[jdx][col_idx]
                 num_ops += 1
             row.append(total)
         ans.append(row)
+
+    print(f"{num_ops=}")
+
+    return ans
+
+
+def get_sparse_matrix(matrix):
+    sparse_version = dict()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                sparse_version[(i, j)] = matrix[i][j]
+
+    return sparse_version
+
+
+def sparse_matrix_multiplication2(matrix_a, matrix_b):
+    """Proposal"""
+    a_num_rows = len(matrix_a)
+    a_num_cols = len(matrix_a[0])
+
+    b_num_rows = len(matrix_b)
+    b_num_cols = len(matrix_b[0])
+
+    if a_num_cols != b_num_rows:
+        return [[]]
+
+    sparse_matrix_a = get_sparse_matrix(matrix_a)
+    sparse_matrix_b = get_sparse_matrix(matrix_b)
+
+    # result matrix has shape: a_num_rows * b_num_cols
+    ans = [[0] * b_num_cols for _ in range(a_num_rows)]
+
+    num_ops = 0
+    for row_a, col_a in sparse_matrix_a.keys():
+        for col_b in range(b_num_cols):
+            if (col_a, col_b) in sparse_matrix_b:
+                ans[row_a][col_b] += sparse_matrix_a[(row_a, col_a)] * sparse_matrix_b[(col_a, col_b)]
+                num_ops += 1
 
     print(f"{num_ops=}")
 
@@ -59,3 +99,4 @@ if __name__ == "__main__":
         b.append(tmp)
 
     print(sparse_matrix_multiplication(a, b))
+    print(sparse_matrix_multiplication2(a, b))
